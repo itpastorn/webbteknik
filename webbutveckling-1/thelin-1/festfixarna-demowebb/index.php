31c0e4272772d1125018cf3c3dd1d26f3218002d
Lars Gunther <gunther@keryx.se>
2012-01-13 00:13:22 +0100

    Demosajt + SVG-klocka + fix på flashcards

---
+++ webbutveckling-1/thelin-1/festfixarna-demowebb/index.php
@@ -0,0 +1,43 @@
+<?php
+/* Detta är en startsidan till demo-webbplatsen för läroboken Webbutveckling 1
+ *
+ * Det tänkta företaget 
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+?>
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+  <meta charset="utf-8" />
+  <title>Startsida | Festfixarna</title>
+  <link href='http://fonts.googleapis.com/css?family=Merienda+One' rel='stylesheet' />
+  <link href="festfixarna.css" rel="stylesheet" />
+</head>
+<body class="startpage">
+  <header>
+    <img id="logo" alt="" src="" />
+    <span class="imagefiller">&#x2619;</span>
+    <h1>Festfixarna</h1>
+    <p class="tagline">Fester och events utöver det vanliga</p>
+  </header>
+  <section>
+    <h2>Vilka vi är &#x3020;</h2>
+    <p>Varför anlita oss? Lorem ipsum dolor sit amet.</p>
+  </section>
+  <section>
+    <h2>Vad vi gör &#x2318;</h2>
+    <p>En fest ska inte bara vara rolig, utan minnesvärd! Lorem ipsum dolor sit amet.</p>
+  </section>
+  <section>
+    <h2>Blogg &#x272A;</h2>
+    <p>Möt våra nöjda kunder och se exempel på vad vi gjort åt dem.</p>
+  </section>
+  <section>
+    <h2>Kontakt &#x260E;</h2>
+    <p>Varför anlita oss? Lorem ipsum dolor sit amet.</p>
+  </section>
+  <footer>
+    <small>&copy; Lars Gunther och Thelin AB</small>
+  </footer>
+</body>
+</html>
