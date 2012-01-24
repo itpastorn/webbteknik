51f6d2c7e7be2ea019327ad8616a79ad30482fba
Lars Gunther <gunther@keryx.se>
2012-01-24 16:49:29 +0100

    added to kursvalen

--- future-stuff/inriktningsval/inriktningar-och-paket.js
+++ future-stuff/inriktningsval/inriktningar-och-paket.js
@@ -9,11 +9,11 @@ var inriktingar = [
     },
     {
         id : "produktion",
-        name: "Design och produktutveckling",
+        name: "xxxx",
         blockar : [],
-        passar : "",
+        passar : "extra_it",
         typ : "produktion",
-        kurser : []
+        kurser : ["webbutv1", "prog1", "dao"]
     },
     {
         id : "it",
@@ -34,8 +34,8 @@ var inriktingar = [
 ];
 
 var kurser = {
-    "dao" : { // Full kod sedan
-        url : "", // Kan den skapas från koden?
+    "webweu01" : { // Full kod sedan
+    "url" : "http://www.skolverket.se/forskola-och-skola/gymnasieutbildning/amnes-och-laroplaner/amnesplaner-och-kurser-for-gymnasieskolan-2011/subject.htm?subjectCode=WEB&courseCode=WEBWEU01#anchor_WEBWEU01", // Kan den skapas från koden?
         
     },
 };
@@ -43,10 +43,10 @@ var kurser = {
 // Byggs utifrån "paket" tabellen
 var paket1 = [
     "civing" : ["ma4", "fy2"],
-    "it4"    : ["gdesign", "ws1"],
+    "it4"    : ["gdesign", "ws1"]
 ];
 var paket2 = [
     "civing" : ["ma4", "fy2"],
-    "it4"    : ["gdesign", "ws1"],
+    "it4"    : ["gdesign", "ws1"]
 ];
 
