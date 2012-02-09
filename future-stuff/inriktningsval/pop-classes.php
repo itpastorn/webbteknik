1436ac43a1e852cc13cc226f6067dba162c10903
Lars Gunther <gunther@keryx.se>
2012-02-09 23:11:31 +0100

    Set up SQL

--- future-stuff/inriktningsval/pop-classes.php
+++ future-stuff/inriktningsval/pop-classes.php
@@ -13,4 +13,19 @@ require_once("dbcx.php");
 $dbh = dbcx();
 
 $elever = file("te1b-2012.cvs");
-var_dump($elever);
+$elever = array_merge(file("te1a-2012.cvs"), $elever);
+
+// echo "<pre>";
+// var_dump($elever);
+// klass,efternamn, fornamn,personnummer
+
+
+$sql  = "INSERT INTO elever (personnummer, fornamn, efternamn, klass, kod, year) ";
+$sql .= "VALUES (:personnummer, :fornamn, :efternamn, :klass, :kod, :year)";
+$stmt = $dbh->prepare($sql);
+$stmt->bindParam(":personnummer", $personnummer);
+$stmt->bindParam(":fornamn", $fornamn);
+$stmt->bindParam(":efternamn", $efternamn);
+$stmt->bindParam(":klass", $klass);
+$stmt->bindParam(":kod", $kod);
+
