a9c92f1c51d61be32c1e0ea5c0dec520df516e4f
Lars Gunther <gunther@keryx.se>
2012-02-10 01:02:40 +0100

    Utskriftssida användbar (beta)

--- future-stuff/inriktningsval/printcodes.php
+++ future-stuff/inriktningsval/printcodes.php
@@ -18,5 +18,41 @@ $dbh = dbcx();
 
 // Borde det finnas en "vad är min kod" funktion? JA - via mejl!
 
-$stmt = $dbh->prepare("SELECT fornamn, efternamn, klass, kod FROM elever ORDER BY klass ASC efternamn ASC");
+$stmt = $dbh->prepare("SELECT fornamn, efternamn, klass, kod FROM elever ORDER BY klass ASC, efternamn ASC");
+$stmt->execute();
 
+$data = "";
+while ( $e = $stmt->fetch() ) {
+    $data .= "<tr><td>{$e['fornamn']}</td><td>{$e['efternamn']}</td>" .
+            "<td>{$e['klass']}</td><td>{$e['kod']}</td><tr>";
+}
+
+// Sidmall
+?>
+<!DOCTYPE html>
+<html lang="sv">
+ <head>
+  <meta charset="utf-8" />
+  <title>För utskrift: Personliga koder</title>
+  <style>
+    body {
+        font-family: Calibri, sans-serif;
+        font-size: 13pt;
+    }
+    table, td {
+        padding: 0.7cm 0.2cm;
+        border-top: 1px solid silver;
+        border-bottom: 1px solid silver;
+        border-collapse: collapse;
+    }
+    td:last-of-type {
+        padding-left: 2cm;
+    }
+  </style>
+ </head>
+ <body>
+   <h1>För utskrift: Personliga koder</h1>
+   <table>
+     <?php echo $data; ?>
+   </table>
+   <p>Fattas det någon? Är det något fel?</p>
