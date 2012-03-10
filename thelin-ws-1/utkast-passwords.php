eebf7e8fe07839fc35ff71ca5b50e7af89452579
Lars Gunther <gunther@keryx.se>
2012-03-10 01:31:18 +0100

    Utförligare pw-demo

--- thelin-ws-1/utkast-passwords.php
+++ thelin-ws-1/utkast-passwords.php
@@ -1,14 +1,23 @@
 <?php
 /**
  * Saltade hashade lösenord
+ * 
+ * Visar *principen* för hur man kan skapa lösenord
+ * Visar *principen* för hur man kan testa lösenord
  */
 
-// password-security.php
+// Förslag på filnamn: password-security.php
 
 // Skapa lösenord med SHA512 som algoritm
 
+// Unikt salt
 $salt = uniqid('', true); // Fler än 16 slumpmässiga tecken
+// Vi låtsas att detta kommer från användaren och genomgått grundläggande indatafiltrering
 $user_submitted_password = "hejsan"; // svagt lösen
+// Vi låtsas också att detta önskade användarnamn kommer från användaren och redan filtrerats
+$username = "testuser";
+
+// Skapa lösenordet med crypt()
 $password = crypt(
     $user_submitted_password,
     '$6$' . $salt . '$'
@@ -21,41 +30,58 @@ $stmt = $dbh->prepare("
     INSERT INTO users (username, password)
     VALUES (:username, :password)
 ");
-$stmt->bind(":username", $username); // Från formulär
-$stmt->bind(":password", $password); // Från crypt
+$stmt->bindParam(":username", $username);
+$stmt->bindParam(":password", $password); // Från crypt
 $stmt->execute();
 
+// -------------------------------------------------------------------------------------------------
 
 // Testa lösenord (inloggning)
+
+// Vi låtsas på nytt att detta önskade användarnamn kommer från användaren och redan filtrerats
+$username = "testuser";
+// Ditto för lösenordet
+$user_submitted_password = "hejsan";
+
 // Hämta lösen (det riktiga) ur databasen
+// Du skulle också kunna passa på att hämta mer info att använda om inloggningen lyckats
 $stmt = $dbh->prepare("SELECT password FROM users WHERE username = :username");
-$stmt->bind(":username", $username); // från formulär
+$stmt->bind(":username", $username);
 $stmt->execute();
 $userdata = $stmt->fetch(); // $userdata är nu en array eller false
 
 if ( empty($userdata) ) {
-    // No such user...
+    // Användaren existerar inte alls i databasen
+    // Vad göra? Visa lämpligt felmeddelande och avbryt
+    // .........
+    exit;
 }
 
-// Lägg märke till att lösenordet innehåller saltet. därför är det parameter till crypt()
+// Lägg märke till att lösenordet innehåller saltet. Därför är det parameter till crypt()
 if ( crypt($user_submitted_password, $userdata['password']) == $userdata['password'] ) {
     // Hurra inloggad
+    // Ställ in sessionsvariabler
+    // Ev. omdirigera med header("Location: ....")
+    
 }
 
 /*
- * Alternativ lösning som inte funkar på Windows som saknar crypt()
+ * Alternativ lösning som inte funkar på Windows som saknar stöd för crypt() i operativsystemet
+ * Annars är denna måhända något elegantare
  */
 $stmt = $dbh->prepare(
     "SELECT password FROM users WHERE username = :username AND ENCRYPT(:upassword, password) = password"
 );
-$stmt->bind(":username", $username); // från formulär
-$stmt->bind(":upassword", $user_submitted_Password); // från formulär
+$stmt->bind(":username", $username);
+$stmt->bind(":upassword", $user_submitted_Password);
 $stmt->execute();
 $userdata = $stmt->fetch(); // $userdata är nu en array eller false
 
 ?>
-// Lösenordslängd i DB 106 tecken
-// Databasens struktur - tabell users
+-- SQL för matchande DB
+-- Lösenordslängd i DB 106 tecken
+-- Databasens struktur - tabell users
+
 username varchar(20) not null PRIMARY_KEY
 password varchar(106) not null
 
