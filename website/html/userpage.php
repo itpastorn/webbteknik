50ed21741fcf04feb8828a40598809cf025cd3d4
Lars Gunther <gunther@keryx.se>
2012-08-13 21:04:33 +0200

    Remove reduntant ORDER BY, add note about video numbering

--- website/html/userpage.php
+++ website/html/userpage.php
@@ -37,12 +37,12 @@ if ( isset($_GET['video']) ) {
         LEFT JOIN userprogress AS up
         ON (jl.joblistID = up.joblistID)
         WHERE v.videoname = :video AND ( up.email = :email or up.email IS NULL )
-        ORDER BY jl.chapter ASC, jl.slow_track_order ASC
 SQL;
     $stmt = $dbh->prepare($sql);
     $stmt->bindParam(':video', $_GET['video']);
 } elseif (isset($_GET['vidnum']) ) {
 	// TODO: Change to use job number, video table should not have any suggested order
+	// Problem with that is that we can not do+/- 1 to get previous and next
 	$vidnum = (int)$_GET['vidnum'];
     $sql = <<<SQL
         SELECT v.*, jl.joblistID, up.progressdata, up.percentage_complete, up.status
@@ -52,7 +52,6 @@ SQL;
         LEFT JOIN userprogress AS up
         ON (jl.joblistID = up.joblistID)
         WHERE v.order = :vidnum AND ( up.email = :email or up.email IS NULL )
-        ORDER BY jl.chapter ASC, jl.slow_track_order ASC
 SQL;
     $stmt = $dbh->prepare($sql);
     $stmt->bindParam(':vidnum', $vidnum);
