a0b3e52f2013d093812276853b65b4c4a56b3b20
Lars Gunther <gunther@keryx.se>
2012-08-14 22:24:48 +0200

    dump users without any mail addresses

---
+++ website/html/api/dump_users.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * List of users, to used for sending email
+ *
+ */
+
+session_start();
+require_once '../../includes/loadfiles.php';
+
+user::setSessionData();
+
+user::requires(user::ADMIN);
+
+// Database settings and connection
+$dbx = config::get('dbx');
+// init
+$dbh = keryxDB2_cx::get($dbx);
+
+header("Content-type: text/plain; charset=utf-8");
+
+foreach ( $dbh->query("SELECT * FROM users") as $row) {
+    echo "{$row['firstname']} {$row['lastname']} <{$row['email']}>\n";
+}
