d154b7e0d3271e83673a6ccf200ddd046bb9e936
Lars Gunther <gunther@keryx.se>
2012-08-17 02:22:24 +0200

    Showing what user is logged in

--- website/includes/snippets/mainmenu.php
+++ website/includes/snippets/mainmenu.php
@@ -1,4 +1,15 @@
   <nav class="mainmenu">
+    <p class="userdata">    
+<?php
+if ( isset($_SESSION['userdata']) ) {
+    echo <<<USERDATA
+    {$_SESSION['userdata']->firstname}
+    {$_SESSION['userdata']->lastname}
+    ({$_SESSION['userdata']->email})
+USERDATA;
+}
+?>
+    </p>
     <ul>
       <li><a href="./">Startsidan</a></li>
       <li><a href="userpage.php">Arbetssida</a></li>
