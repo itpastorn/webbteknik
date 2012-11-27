6b1e8fdbd80a788dfd4392e42a933182447469c0
Lars Gunther <gunther@keryx.se>
2012-11-27 11:42:56 +0100

    New message for users after last update

--- website/html/api/contact_all_users.php
+++ website/html/api/contact_all_users.php
@@ -25,47 +25,22 @@ echo "Påbörjar utskick\n";
 
 $text = <<<TXT
 
-Hej lärare med elever på webbteknik.nu
+Hej alla som använder webbteknik.nu
 
-Under veckan som gick fick jag några rapporter om att
-videos inte laddade som de skulle. Jag tror att problemet är
-löst nu.
+Under natten så uppstod ett tekniskt strul på servern som jag hoppas ska vara åtgärdat nu.
 
-Skriver man
+Jag har kommunicerat med webbhotellet och hoppas inom några dagar fixa detta, så att det inte
+kan återupprepas.
 
-www.webbteknik.nu
+Några nya filmer har kommit upp. Ni når dem via kapitel 9 och 10 i arbetsplaneringen.
 
-så ska man bli omdirigerad till
+Inloggningen ska förhoppningsvis vara enklare nu. Webbplatsen ska komma ihåg era inloggningar,
+så länge som ni är inloggad på Persona-systemet. Utloggningsfunktion, som efterlysts
+av somliga, är på G.
 
-webbteknik.nu (utan www)
-
-Detta funkade inte. Bara de första kapitlens video funkar med www i adressen.
-
-
-Om det fortfarande strular, så hjälp mig helst åtgärda felet snabbt
-med en detaljerad felrapport:
-
-1. Exakt vilken video är det som inte kan ses?
-
-2. Kolla i konsollen vad som står där.
-
-3. På Chrome, kolla också "Nät". Jag behöver se HTTP-anropen.
-
-
-Elever som skrivit adressen med "www" kan behöva logga in på nytt.
-Inloggningen sker per domän.
-
-
-Snabb och svår teknisk förklaring för den hågade:
-
-Omdirigeringen slutade fungera häromveckan, då servern fick nya default-regler för mod_rewrite
-
-Så här ser regeln ut i min .htaccess-fil, där rad 1 nu har lagts till
-
-RewriteEngine On
-RewriteCond %{HTTP_HOST} ^www\.webbteknik\.nu$ [NC]
-RewriteRule ^(.*)$ http://webbteknik.nu/$1 [R=301,L]
-
+Skulle något nu strula, så hör av er. Som vanligt underlättas min felsökning om 
+ni berättar vilken webbläsare ni använder och vad som syns i konsollen när ni försöker
+göra något.
 
 
 mvh
@@ -90,7 +65,7 @@ error_reporting(E_ALL);
 ini_set("display_errors", "1");
 echo "UTF-8\n";
 
-foreach ( $dbh->query($teachers) as $row) {
+foreach ( $dbh->query($all_users) as $row) {
 	$to = "{$row['firstname']} {$row['lastname']} <{$row['email']}>";
     if (mail($to, $subject, $text, $headers) ) {
         echo "{$to} kontaktad\n";
