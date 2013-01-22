a3f28014ff0137c1a6584e4193fb15066e6023fa
Lars Gunther <gunther@keryx.se>
2013-01-22 22:54:24 +0100

    new messafe to users

--- website/html/api/contact_all_users.php
+++ website/html/api/contact_all_users.php
@@ -19,47 +19,23 @@ $dbh = keryxDB2_cx::get($dbx);
 
 header("Content-type: text/plain; charset=utf-8");
 
-$subject = "God jul lärare på webbteknik.nu";
+$subject = "Alla videos till kapitel 13 klara";
 
 echo "Påbörjar utskick\n"; 
 
 $text = <<<TXT
 
-Hej alla ni lärare som använder mitt material.
+Hej
 
-Låt mig börja med att be om ursäkt för alla förseningar med CD:n.
+God fortsättning.
 
-En tanke är att all kod som används till videofilmerna ska vara med
-och när filmerna dröjer, så dröjer också CD:n.
+Jag hann inte riktigt göra alla videos
+klart över jullovet, men fem nya blev det.
 
-Ni kan titta på koden här för alla klara filmer:
+Nu är alla videos till kapitel 13 klara och dessutom
+finns övningsfilerna för att integrera formuläret med PHP.
 
-https://github.com/itpastorn/webbteknik/tree/master/webbutveckling-1/videos-kod
-
-Klicka på ett filnamn och klicka sedan på knappen "raw", så kan ni hämta er egen kopia.
-
-
-Jag förstår frustrationen över att saker drar ut på tiden. Jag är nog mest frusterad av
-oss alla över detta!
-
-När vårterminen drar igång, så är min förhoppning att resterande filmer ska vara gjorda.
-
-
-Jag har i min mailbox ett litet antal oavslutade supportärenden. Någon har bett om hjälp,
-och då mötts av följdfrågor från min sida. Men sedan har det blivit tyst. Jag antar att
-problemen då löst sig.
-
-Det vanligaste strulet är med inloggningen. Som ni kanske märkt är tekniken nu
-enklare, då webbläsaren kommer ihåg att man är inloggad. Men när detta inte är fallet
-så säger några användare att man måste klicka flera gånger. Jag har väldigt svårt
-att rekonstruera problemet. Troligen har det att göra med känslighet för latens.
-
-Ju mer tekniska detaljer jag får kring ett problem, desto enklare är det för mig att
-åtgärda dem, så försök hjälpa era elever med detta. En beskrivning som "jag kan inte logga in"
-eller "jag ser inte filmen" ger mig ingen vägledning alls.
-
-
-I övrigt önskar jag också er GOD JUL!
+http://webbteknik.nu/assignments/
 
 
 Lars Gunther
@@ -83,7 +59,7 @@ error_reporting(E_ALL);
 ini_set("display_errors", "1");
 echo "UTF-8\n";
 
-foreach ( $dbh->query($teachers) as $row) {
+foreach ( $dbh->query($all_users) as $row) {
 	$to = "{$row['firstname']} {$row['lastname']} <{$row['email']}>";
     if (mail($to, $subject, $text, $headers) ) {
         echo "{$to} kontaktad\n";
