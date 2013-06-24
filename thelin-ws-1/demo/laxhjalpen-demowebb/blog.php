41a6d522ff99c4d449876ff160f51645a7e90f8b
Lars Gunther <gunther@keryx.se>
2013-06-24 17:46:37 +0100

    Laxhjalpen till ws-1 boken

---
+++ thelin-ws-1/demo/laxhjalpen-demowebb/blog.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+  <meta charset="utf-8" />
+  <title>Blog - Läxhjälpen</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link href='http://fonts.googleapis.com/css?family=Merienda+One' rel='stylesheet' />
+  <link href="laxhjalpen.css" title="Finished" rel="stylesheet" />
+</head>
+<body class="subpage">
+  <header>
+    <div class="hwrap">
+      <a href="./"><img class="logo" alt="Startsidan" src="laxlogo.png" /></a>
+      <h1>Läxhjälpen: <span>Blogg</span></h1>
+    </div>
+   <hr />
+  </header>
+  <nav>
+    <ul class="menu">
+      <li><a href="om-oss.html">Vilka vi är &#x3020;</a></li>
+      <li><a href="arbeten.html">Vad vi gör &#x2318;</a></li>
+      <li><a href="blog.php">Blogg &#x272A;</a></li>
+      <li><a href="contact.php">Kontakt &#x260E;</a></li>
+    </ul>
+  </nav>
+  <div role="main">
+    <p>Sidan kommer till boken i Webbserverprogrammering våren 2013.</p>
+  </div>
+  <footer>
+    <small>&copy; Lars Gunther och Thelin AB</small>
+  </footer>
+</body>
+</html>
